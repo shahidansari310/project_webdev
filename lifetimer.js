82b9bfd827c53686dd3e40f6ef3d23a1b442f17c
Shahid Ansari <shahidansaribhodu@gmail.com>
2024-11-17 17:18:17 +0530

    added timer

--- lifetimer.js
+++ lifetimer.js
@@ -1,14 +1,20 @@
 let isDOBOpen=false;
-let dob
+let dateofbirth;
 const cogel=document.getElementById("cog");
 const scontel=document.getElementById("scont");
 const initialel=document.getElementById("initial");
 const afterel=document.getElementById("after");
 const dobButtonel=document.getElementById("dobButton");
 const dobInputel=document.getElementById("dobInput");
-
-
-
+const yearel=document.getElementById("year");
+const monthel=document.getElementById("month");
+const dayel=document.getElementById("day");
+const hourel=document.getElementById("hour");
+const minel=document.getElementById("min");
+const secel=document.getElementById("sec");
+const maketwo=(number)=>{
+    return number > 9 ? number : `0${number}`;
+};
 const toggle = () => {
     if (isDOBOpen) {
         scontel.classList.add("hide");
@@ -19,11 +25,45 @@ const toggle = () => {
     console.log("toggle", isDOBOpen);
 };
 
-const setdobhandler =() =>{
-    dateofbirth=dobInputel.value;
-    console.log(dateofbirth);
+const updateage=() =>{
+    const currenDate= new Date();
+    const datediff=currenDate-dateofbirth;
+    const year=Math.floor(datediff/(1000*60*60*24*365));
+    const month=Math.floor(datediff/(1000*60*60*24)%12);
+    const day=Math.floor(datediff/(1000*60*60*24))%30;
+    const hour=Math.floor(datediff/(1000*60*60))%24;
+    const min=Math.floor(datediff/(1000*60))%60;
+    const sec=Math.floor(datediff/(1000))%60;
+    
+    yearel.innerHTML=maketwo(year);
+    monthel.innerHTML=maketwo(month);
+    dayel.innerHTML=maketwo(day);
+    hourel.innerHTML=maketwo(hour);
+    minel.innerHTML=maketwo(min);
+    secel.innerHTML=maketwo(sec);
 
-}
+};
+
+
+const setdobhandler =() =>{
+    const datestring =dobInputel.value;
+    
+    dateofbirth=datestring ? new Date(datestring) : null;
+
+    if(dateofbirth){
+        initialel.classList.add("hide");
+        afterel.classList.remove("hide");
+        setInterval(()=>updateage(),100);
+    }
+    else{
+        afterel.classList.add("hide");
+        initialel.classList.remove("hide");
+    }
+    
+    
+};
+
+setdobhandler();
 
 cogel.addEventListener("click",toggle);
 dobButtonel.addEventListener("click",setdobhandler);
