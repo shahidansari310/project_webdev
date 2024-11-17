4494db712c8a61097807a605b2becbbd55d916ae
Shahid Ansari <shahidansaribhodu@gmail.com>
2024-11-17 15:51:59 +0530

    fixed error of button

--- lifetimer.js
+++ lifetimer.js
@@ -1,6 +1,13 @@
 let isDOBOpen=false;
+let dob
 const cogel=document.getElementById("cog");
 const scontel=document.getElementById("scont");
+const initialel=document.getElementById("initial");
+const afterel=document.getElementById("after");
+const dobButtonel=document.getElementById("dobButton");
+const dobInputel=document.getElementById("dobInput");
+
+
 
 const toggle = () => {
     if (isDOBOpen) {
@@ -12,4 +19,11 @@ const toggle = () => {
     console.log("toggle", isDOBOpen);
 };
 
+const setdobhandler =() =>{
+    dateofbirth=dobInputel.value;
+    console.log(dateofbirth);
+
+}
+
 cogel.addEventListener("click",toggle);
+dobButtonel.addEventListener("click",setdobhandler);
