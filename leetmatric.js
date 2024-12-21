63665b6d74d8726d6d5f69f191d1a9a0aa1b4ba2
Shahid Ansari <shahidansaribhodu@gmail.com>
2024-12-21 20:45:04 +0530

    JS started

---
+++ leetmatric.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoader",function(){
+    const searchbutton=document.getElementById("search");
+    const username=document.getElementById("userr");
+    const statscont=document.querySelector(".")
+})
