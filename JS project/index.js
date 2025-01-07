4afdd8a21493398eede8e37091a4a3c99bd9c033
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-01-07 15:17:07 +0530

    for savng data on website

--- JS project/index.js
+++ JS project/index.js
@@ -1,42 +1,50 @@
 //Logic for table
-let t=document.querySelector("table")
-let data=localStorage.getItem("passwords")
-if(data==null){
-    t.innerHTML="No data found"
-}
-else{
-    let arr=JSON.parse(data)
-    for(let i=0;i<arr.length;i++){
-        const element=arr[i];
+const s=() => {
+    let t = document.querySelector("table")
+    let data = localStorage.getItem("passwords")
+    if (data == null) {
+        t.innerHTML = "No data found"
+    }
+    else {
+        t.innerHTML = `<tr>
+                <th>Website</th>
+                <th> Username</th>
+                <th>Password</th>
+                <th>Delete</th>
+            </tr>`
+        let arr = JSON.parse(data)
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
 
 
-str = `<tr>
+            str = `<tr>
     <td>${element.website}</td>
     <td>${element.username}</td>
     <td>${element.password}</td>
     <td>${"Delete"}</td>
-</tr>`
-t.innerHTML=t.innerHTML+str
+    </tr>`
+            t.innerHTML = t.innerHTML + str
+        }
+    }
 }
-}
-
 
 console.log("Working");
-document.querySelector(".Submit").addEventListener("click",(e)=>{
+s()
+document.querySelector(".Submit").addEventListener("click", (e) => {
     e.preventDefault();
     console.log("Clicked")
-    console.log(username.value, password.value)
-    let passwords=localStorage.getItem("passwords")
+    console.log(website.value, username.value, password.value)
+    let passwords = localStorage.getItem("passwords")
     console.log(passwords)
-    if(passwords==null){
-        let json=[]
-        json.push({username:username.value,password:password.value})
-        localStorage.setItem("passwords",JSON.stringify(json))
+    if (passwords == null) {
+        let json = []
+        json.push({ website: website.value, username: username.value, password: password.value })
+        localStorage.setItem("passwords", JSON.stringify(json))
     }
-    else{
-        let json=JSON.parse(passwords)
-        json.push({username:username.value,password:password.value})
-        localStorage.setItem("passwords",JSON.stringify(json))
+    else {
+        let json = JSON.parse(passwords)
+        json.push({ website: website.value, username: username.value, password: password.value })
+        localStorage.setItem("passwords", JSON.stringify(json))
     }
-
+s();
 })
