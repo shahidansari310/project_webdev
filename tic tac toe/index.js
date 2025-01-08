2efc5720f235c29afe889a797142eefe43c4f8e7
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-01-08 22:19:05 +0530

    declared variables

---
+++ tic tac toe/index.js
@@ -0,0 +1,16 @@
+console.log("Welcome to tic tac toe");
+let music=new Audio("music.mp3");
+let turnm =new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+let turn="X";
+
+const changeturn = () =>{
+    return turn==="X"?"0":"X";
+}
+
+//function to check win
+const checkwin =()=>{
+
+}
+
+//game logic
