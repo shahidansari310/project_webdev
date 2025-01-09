6d528157c71b1481bf8cf445eff9ca66a84d4c32
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-01-09 23:23:24 +0530

    data types

--- abcd.js
+++ abcd.js
@@ -1,12 +1,32 @@
-alert("Hello World");
-console.log("Hello World");
-// prompt("Hello World");
+// alert("Hello World");
+// console.log("Hello World");
+// // prompt("Hello World");
 
-var a=prompt("Enter your name");
-var b=confirm("Are you sure you want to enter your name?");
-if(a==""){
-    alert("Please enter your name"); 
-}
+// var a=prompt("Enter your name");
+// var b=confirm("Are you sure you want to enter your name?");
+// if(a==""){
+//     alert("Please enter your name"); 
+// }
 
-document.write("Your name is : " + a);
-document.title="Hello World";
+// document.write("Your name is : " + a);
+// document.title="Hello World";
+
+var a=5;
+var b="Shahid";
+let c=7;
+const d=10;
+console.log(typeof a , typeof b ,typeof c);
+
+
+// let a=document.getElementsByClassName("light-mode");
+// let b=document.getElementsByClassName("dark-mode");         
+                          
+                                                                          
+let f ={
+    name:"Shahid",
+    age:22,
+    city:"Mumbai",
+    country:"India",
+} 
+console.log(f);      
+                            
