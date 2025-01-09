40f3fce79013705f943f578faeba4fa42bcbb1ed
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-01-09 22:29:08 +0530

    alert prompt

---
+++ abcd.js
@@ -0,0 +1,12 @@
+alert("Hello World");
+console.log("Hello World");
+// prompt("Hello World");
+
+var a=prompt("Enter your name");
+var b=confirm("Are you sure you want to enter your name?");
+if(a==""){
+    alert("Please enter your name"); 
+}
+
+document.write("Your name is : " + a);
+document.title="Hello World";
