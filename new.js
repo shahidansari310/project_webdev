4d36dbcc96b8d49a6e7ac7ac93dd635df13a6396
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-01-10 23:22:41 +0530

    faulty calculator

--- new.js
+++ new.js
@@ -1,1 +1,21 @@
 // create a faulty calculator
+
+let random=Math.random()
+let a=prompt("Enter 1st :")
+let b=prompt("enter 2 :")
+let c=prompt("enter opertaion :")
+
+let obj = {
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"**",
+}
+if (random>0.1){
+    alert(eval(`${a} ${c} ${b}`));
+}
+else{
+    c=obj[c];
+    alert(eval(`${a} ${c} ${b}`));
+
+}
