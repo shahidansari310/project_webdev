6453096ce2731d78aad6517cb92ee133591bb6a3
Shahid Ansari <113099652+shahidansari310@users.noreply.github.com>
2025-01-11 23:50:11 +0530

    answer of ques11

--- exercise11.js
+++ exercise11.js
@@ -7,11 +7,28 @@ Write a program to calculate factorial of a number using reduce an for loops
 
 
 
-console.log("hii");
-
-let a=prompt("enter a number :");
-for(let i=1;i<a;i++) {
-    b=i*(i+1)
-    
-    console.log(b);
+ console.log("hii");
+let b=5;
+//  let a=prompt("enter a number :");
+function fa(number){
+    let r=1
+    for(let i=1; i<=number ;i++) {
+         r=r*i
+    } 
+    //  return r
+    console.log(r)
 }
+fa(b)
+
+// let b=5;
+// const a=prompt("enter a number");
+// function fact(number){
+//    let arr= Array.from(Array(number+1).keys());
+//    console.log(arr.slice(1,));
+//    let c =arr.slice(1,).reduce((a,b)=>{
+//     return a*b
+//    })
+//    return c
+// }
+
+// fact(b);
