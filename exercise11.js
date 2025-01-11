82ffa254c152141d1c2c54b343322eca816942e4
Shahid Ansari <shahidansaribhodu@gmail.com>
2025-01-11 15:52:57 +0530

    ques11 half sol

--- exercise11.js
+++ exercise11.js
@@ -1,9 +1,17 @@
 /*
 Write a program to calculate factorial of a number using reduce an for loops 
+5!=1*2*3*4*5
+
 */
+ 
+
+
+
+console.log("hii");
 
 let a=prompt("enter a number :");
-for (i>0;i<a;i++) {
-    return a*i;
+for(let i=1;i<a;i++) {
+    b=i*(i+1)
     
+    console.log(b);
 }
